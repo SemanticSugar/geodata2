f075ba9808296bf2eec74b9fe351e7cdfa9afcce
Brujo Benavides <fernando.benavides@adroll.com>
2020-04-22 11:15:26 +0200

    Move the project into rebar3 and use all the goodies that come with it

--- src/geodata2_sup.erl
+++ src/geodata2_sup.erl
@@ -1,15 +1,16 @@
 -module(geodata2_sup).
+
 -behaviour(supervisor).
 
 %% API
 -export([start_link/0]).
-
 %% supervisor behavior
 -export([init/1]).
+
 -include("geodata2.hrl").
 
 start_link() ->
-	supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
 init([]) ->
     RestartStrategy = one_for_one,
@@ -20,6 +21,9 @@ init([]) ->
     ChildName = child_proc,
     ChildSpec = {ChildName,
                  {geodata2, start_link, [ChildName]},
-                 permanent, 5000, worker, [geodata2]},
+                 permanent,
+                 5000,
+                 worker,
+                 [geodata2]},
 
     {ok, {SupFlags, [ChildSpec]}}.
