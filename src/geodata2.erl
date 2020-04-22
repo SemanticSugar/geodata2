762ffcc024d71ff57170c4a8fba360f5468fb605
Brujo Benavides <fernando.benavides@adroll.com>
2020-04-22 14:38:31 +0200

    [RTI-7894] Move the project into rebar3 and use all the goodies that come with it (#3)
    * Move the project into rebar3 and use all the goodies that come with it
    
    * Some formatting improvements

--- src/geodata2.erl
+++ src/geodata2.erl
@@ -1,60 +1,64 @@
 -module(geodata2).
+
 -behavior(gen_server).
 
 %% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
-
+-export([init/1, handle_call/3, handle_cast/2]).
 %% API
 -export([lookup/1, start/0, start_link/1, stop/0, get_env/2, id/1]).
+
 -include("geodata2.hrl").
 
 -record(state, {}).
 
--spec lookup(any()) -> {ok, Result::list()} | not_found | {error, Reason :: term()}.
+-type state() :: #state{}.
+
+-spec lookup(any()) -> {ok, Result :: list()} | not_found | {error, Reason :: term()}.
 lookup(IP) ->
     [{data, Data}] = ets:lookup(?GEODATA2_STATE_TID, data),
     [{meta, Meta}] = ets:lookup(?GEODATA2_STATE_TID, meta),
     case geodata2_ip:make_ip(IP) of
-        {ok, Bits, IPV} ->
-            geodata2_format:lookup(Meta, Data, Bits, IPV);
-        {error, Reason} ->
-            {error, Reason}
+      {ok, Bits, IPV} ->
+          geodata2_format:lookup(Meta, Data, Bits, IPV);
+      {error, Reason} ->
+          {error, Reason}
     end.
 
 start() ->
-	application:start(geodata2).
+    application:start(geodata2).
 
 start_link(Name) ->
     gen_server:start_link({local, Name}, ?MODULE, [], []).
 
 stop() ->
-	application:stop(geodata2).
+    application:stop(geodata2).
 
 new(File) ->
     case filelib:is_file(File) of
-        true ->
-            {ok, RawData} = file:read_file(File),
-            Data = case is_compressed(File) of
-                       true ->
-                           zlib:gunzip(RawData);
-                       false ->
-                           RawData
-                   end,
-            {ok, Meta} = geodata2_format:meta(Data),
-            ets:new(?GEODATA2_STATE_TID, [set, protected, named_table, {read_concurrency, true}]),
-            ets:insert(?GEODATA2_STATE_TID, {data, Data}),
-            ets:insert(?GEODATA2_STATE_TID, {meta, Meta}),
-            {ok, #state{}};
-        _ ->
-            {stop, {geodata2_dbfile_not_found, File}}
+      true ->
+          {ok, RawData} = file:read_file(File),
+          Data = case is_compressed(File) of
+                   true ->
+                       zlib:gunzip(RawData);
+                   false ->
+                       RawData
+                 end,
+          {ok, Meta} = geodata2_format:meta(Data),
+          ets:new(?GEODATA2_STATE_TID, [set, protected, named_table, {read_concurrency, true}]),
+          ets:insert(?GEODATA2_STATE_TID, {data, Data}),
+          ets:insert(?GEODATA2_STATE_TID, {meta, Meta}),
+          {ok, #state{}};
+      _ ->
+          {stop, {geodata2_dbfile_not_found, File}}
     end.
 
+-spec init(_) -> {ok, state()} | {stop, tuple()}.
 init(_Args) ->
     case get_env(geodata2, dbfile) of
-        {ok, File} ->
-            new(File);
-        _ ->
-            {stop, {geodata2_dbfile_unspecified}}
+      {ok, File} ->
+          new(File);
+      _ ->
+          {stop, {geodata2_dbfile_unspecified}}
     end.
 
 handle_call(_What, _From, State) ->
@@ -63,33 +67,27 @@ handle_call(_What, _From, State) ->
 handle_cast(_Request, State) ->
     {noreply, State}.
 
-handle_info(_Info, State) ->
-    {noreply, State}.
-
-terminate(_Reason, _State) ->
-    ok.
-
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
-
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
 
 get_env(App, Key) ->
     {ConfigModule, ConfigFun} = case application:get_env(geodata2, config_interp) of
-                                    {ok, {Cm, Cf}} -> {Cm, Cf};
-                                    _              -> {?MODULE, id}
+                                  {ok, {Cm, Cf}} ->
+                                      {Cm, Cf};
+                                  _ ->
+                                      {?MODULE, id}
                                 end,
     case application:get_env(App, Key) of
-        {ok, Value} ->
-            {ok, ConfigModule:ConfigFun(Value)};
-        Other ->
-            Other
+      {ok, Value} ->
+          {ok, ConfigModule:ConfigFun(Value)};
+      Other ->
+          Other
     end.
 
 %% this is used to return app env values as-is when config_interp is not set:
-id(X) -> X.
+id(X) ->
+    X.
 
 is_compressed(Filename) ->
     <<".gz">> == iolist_to_binary(filename:extension(Filename)).
--- src/geodata2_app.erl
+++ src/geodata2_app.erl
@@ -1,11 +1,12 @@
 -module(geodata2_app).
+
 -behaviour(application).
 
 -export([start/2]).
 -export([stop/1]).
 
 start(_Type, _Args) ->
-	geodata2_sup:start_link().
+    geodata2_sup:start_link().
 
 stop(_State) ->
-	ok.
+    ok.
