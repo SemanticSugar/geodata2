69f9182f6da43822b3929fd69d4e57e8635ff27d
Brujo Benavides <fernando.benavides@adroll.com>
2020-09-01 14:06:06 +0200

    Apply latest formatting

--- src/geodata2.erl
+++ src/geodata2.erl
@@ -18,10 +18,10 @@ lookup(IP) ->
     [{data, Data}] = ets:lookup(?GEODATA2_STATE_TID, data),
     [{meta, Meta}] = ets:lookup(?GEODATA2_STATE_TID, meta),
     case geodata2_ip:make_ip(IP) of
-      {ok, Bits, IPV} ->
-          geodata2_format:lookup(Meta, Data, Bits, IPV);
-      {error, Reason} ->
-          {error, Reason}
+        {ok, Bits, IPV} ->
+            geodata2_format:lookup(Meta, Data, Bits, IPV);
+        {error, Reason} ->
+            {error, Reason}
     end.
 
 start() ->
@@ -35,30 +35,31 @@ stop() ->
 
 new(File) ->
     case filelib:is_file(File) of
-      true ->
-          {ok, RawData} = file:read_file(File),
-          Data = case is_compressed(File) of
-                   true ->
-                       zlib:gunzip(RawData);
-                   false ->
-                       RawData
-                 end,
-          {ok, Meta} = geodata2_format:meta(Data),
-          ets:new(?GEODATA2_STATE_TID, [set, protected, named_table, {read_concurrency, true}]),
-          ets:insert(?GEODATA2_STATE_TID, {data, Data}),
-          ets:insert(?GEODATA2_STATE_TID, {meta, Meta}),
-          {ok, #state{}};
-      _ ->
-          {stop, {geodata2_dbfile_not_found, File}}
+        true ->
+            {ok, RawData} = file:read_file(File),
+            Data =
+                case is_compressed(File) of
+                    true ->
+                        zlib:gunzip(RawData);
+                    false ->
+                        RawData
+                end,
+            {ok, Meta} = geodata2_format:meta(Data),
+            ets:new(?GEODATA2_STATE_TID, [set, protected, named_table, {read_concurrency, true}]),
+            ets:insert(?GEODATA2_STATE_TID, {data, Data}),
+            ets:insert(?GEODATA2_STATE_TID, {meta, Meta}),
+            {ok, #state{}};
+        _ ->
+            {stop, {geodata2_dbfile_not_found, File}}
     end.
 
 -spec init(_) -> {ok, state()} | {stop, tuple()}.
 init(_Args) ->
     case get_env(geodata2, dbfile) of
-      {ok, File} ->
-          new(File);
-      _ ->
-          {stop, {geodata2_dbfile_unspecified}}
+        {ok, File} ->
+            new(File);
+        _ ->
+            {stop, {geodata2_dbfile_unspecified}}
     end.
 
 handle_call(_What, _From, State) ->
@@ -72,17 +73,18 @@ handle_cast(_Request, State) ->
 %%%===================================================================
 
 get_env(App, Key) ->
-    {ConfigModule, ConfigFun} = case application:get_env(geodata2, config_interp) of
-                                  {ok, {Cm, Cf}} ->
-                                      {Cm, Cf};
-                                  _ ->
-                                      {?MODULE, id}
-                                end,
+    {ConfigModule, ConfigFun} =
+        case application:get_env(geodata2, config_interp) of
+            {ok, {Cm, Cf}} ->
+                {Cm, Cf};
+            _ ->
+                {?MODULE, id}
+        end,
     case application:get_env(App, Key) of
-      {ok, Value} ->
-          {ok, ConfigModule:ConfigFun(Value)};
-      Other ->
-          Other
+        {ok, Value} ->
+            {ok, ConfigModule:ConfigFun(Value)};
+        Other ->
+            Other
     end.
 
 %% this is used to return app env values as-is when config_interp is not set:
