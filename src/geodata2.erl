2638ced56a917130c0e9ef79a41f9bbda1909508
Bruno Carvalho <bruno.carvalho@adroll.com>
2020-11-04 04:26:54 -0300

    first try at updating geodata2

--- src/geodata2.erl
+++ src/geodata2.erl
@@ -5,7 +5,7 @@
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2]).
 %% API
--export([lookup/1, start/0, start_link/1, stop/0, get_env/2, id/1, is_ipv6_mmdb/0]).
+-export([lookup/1, start/0, start_link/1, stop/0, get_env/2, id/1]).
 
 -include("geodata2.hrl").
 
@@ -18,10 +18,10 @@ lookup(IP) ->
     [{data, Data}] = ets:lookup(?GEODATA2_STATE_TID, data),
     [{meta, Meta}] = ets:lookup(?GEODATA2_STATE_TID, meta),
     case geodata2_ip:make_ip(IP) of
-        {ok, Bits, IPV} ->
-            geodata2_format:lookup(Meta, Data, Bits, IPV);
-        {error, Reason} ->
-            {error, Reason}
+      {ok, Bits, IPV} ->
+          geodata2_format:lookup(Meta, Data, Bits, IPV);
+      {error, Reason} ->
+          {error, Reason}
     end.
 
 start() ->
@@ -33,36 +33,57 @@ start_link(Name) ->
 stop() ->
     application:stop(geodata2).
 
-new(File) ->
-    case filelib:is_file(File) of
-        true ->
-            {ok, RawData} = file:read_file(File),
-            Data =
-                case is_compressed(File) of
-                    true ->
-                        zlib:gunzip(RawData);
-                    false ->
-                        RawData
-                end,
-            {ok, Meta} = geodata2_format:meta(Data),
-            %% @TODO [RTI-8302] This one could be removed after the IPv4+IPv6 MMDB is definitely used
-            set_is_ipv6_mmdb(Meta),
-            ets:new(?GEODATA2_STATE_TID, [set, protected, named_table, {read_concurrency, true}]),
-            ets:insert(?GEODATA2_STATE_TID, {data, Data}),
-            ets:insert(?GEODATA2_STATE_TID, {meta, Meta}),
-            {ok, #state{}};
-        _ ->
-            {stop, {geodata2_dbfile_not_found, File}}
-    end.
+new(MagellanFile, DomainFile) ->
+    MagellanState = case filelib:is_file(MagellanFile) of
+      true ->
+          {ok, RawData} = file:read_file(MagellanFile),
+          Data = case is_compressed(MagellanFile) of
+                   true ->
+                       zlib:gunzip(RawData);
+                   false ->
+                       RawData
+                 end,
+          {ok, Meta} = geodata2_format:meta(Data),
+          ets:new(?GEODATA2_STATE_TID, [set, protected, named_table, {read_concurrency, true}]),
+          ets:insert(?GEODATA2_STATE_TID, {data, Data}),
+          ets:insert(?GEODATA2_STATE_TID, {meta, Meta}),
+          {ok, #state{}};
+      _ ->
+          {stop, {geodata2_dbfile_not_found, MagellanFile}}
+    end,
+    DomainState = case filelib:is_file(DomainFile) of
+      true ->
+          {ok, RawData2} = file:read_file(DomainFile),
+          Data2 = case is_compressed(DomainFile) of
+                   true ->
+                       zlib:gunzip(RawData2);
+                   false ->
+                       RawData2
+                 end,
+          {ok, Meta2} = geodata2_format:meta(Data2),
+          ets:new(?GEODATA2_DOMAIN_TID, [set, protected, named_table, {read_concurrency, true}]),
+          ets:insert(?GEODATA2_DOMAIN_TID, {data, Data2}),
+          ets:insert(?GEODATA2_DOMAIN_TID, {meta, Meta2}),
+          {ok, #state{}};
+      _ ->
+          {stop, {ip_to_domain_dbfile_not_found, DomainFile}}
+    end,
+    {ok, {MagellanState, DomainState}}.
 
 -spec init(_) -> {ok, state()} | {stop, tuple()}.
 init(_Args) ->
-    case get_env(geodata2, dbfile) of
-        {ok, File} ->
-            new(File);
-        _ ->
-            {stop, {geodata2_dbfile_unspecified}}
-    end.
+    MagellanFile = case get_env(geodata2, dbfile) of
+      {ok, File} -> File;
+      _ ->
+          {stop, {geodata2_dbfile_unspecified}}
+    end,
+    DomainFile = case get_env(ip_to_domain, dbfile) of
+      {ok, File2} -> File2;
+      _ ->
+          {stop, {ip_to_domain_dbfile_unspecified}}
+    end,
+    new(MagellanFile, DomainFile).
+
 
 handle_call(_What, _From, State) ->
     {reply, ok, State}.
@@ -75,31 +96,22 @@ handle_cast(_Request, State) ->
 %%%===================================================================
 
 get_env(App, Key) ->
-    {ConfigModule, ConfigFun} =
-        case application:get_env(geodata2, config_interp) of
-            {ok, {Cm, Cf}} ->
-                {Cm, Cf};
-            _ ->
-                {?MODULE, id}
-        end,
+    {ConfigModule, ConfigFun} = case application:get_env(geodata2, config_interp) of
+                                  {ok, {Cm, Cf}} ->
+                                      {Cm, Cf};
+                                  _ ->
+                                      {?MODULE, id}
+                                end,
     case application:get_env(App, Key) of
-        {ok, Value} ->
-            {ok, ConfigModule:ConfigFun(Value)};
-        Other ->
-            Other
+      {ok, Value} ->
+          {ok, ConfigModule:ConfigFun(Value)};
+      Other ->
+          Other
     end.
 
 %% this is used to return app env values as-is when config_interp is not set:
 id(X) ->
     X.
-
-%% @TODO [RTI-8302] This one could be removed after the IPv4+IPv6 MMDB is definitely used
-is_ipv6_mmdb() ->
-    application:get_env(geodata2, ipv6, false).
-
-%% @TODO [RTI-8302] This one could be removed after the IPv4+IPv6 MMDB is definitely used
-set_is_ipv6_mmdb(Meta) ->
-    application:set_env(geodata2, ipv6, geodata2_format:is_ipv6(Meta)).
 
 is_compressed(Filename) ->
     <<".gz">> == iolist_to_binary(filename:extension(Filename)).
