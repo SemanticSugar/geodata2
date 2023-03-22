e904d13718fd0bbe132cad567718643a5b65a44f
Pablo López <tak30@users.noreply.github.com>
2023-03-22 12:11:29 +0100

    Remove support for ipv4 only databases (#45)

--- src/geodata2.erl
+++ src/geodata2.erl
@@ -8,8 +8,7 @@
 %% gen_server callbacks
 -export([init/1, handle_call/3, handle_cast/2]).
 %% API
--export([lookup/1, lookup_iptodomain/1, start/0, start_link/1, stop/0, get_env/2, id/1,
-         is_ipv6_mmdb/0]).
+-export([lookup/1, lookup_iptodomain/1, start/0, start_link/1, stop/0, get_env/2, id/1]).
 
 -define(GEODATA2_STATE_TID, geodata2_state).
 -define(GEODATA2_DOMAIN_TID, geodata2_domain).
@@ -71,12 +70,6 @@ new(ConfigName, Ets) ->
                                 RawData
                         end,
                     {ok, Meta} = geodata2_format:meta(Data),
-                    case ConfigName of
-                        dbfile ->
-                            set_is_ipv6_mmdb(Meta);
-                        _ ->
-                            ok
-                    end,
                     ets:insert(Ets, {data, Data}),
                     ets:insert(Ets, {meta, Meta}),
                     ok;
@@ -132,11 +125,3 @@ id(X) ->
 
 is_compressed(Filename) ->
     <<".gz">> == iolist_to_binary(filename:extension(Filename)).
-
-%% @TODO [RTI-8302] This one could be removed after the IPv4+IPv6 MMDB is definitely used
-is_ipv6_mmdb() ->
-    application:get_env(geodata2, ipv6, false).
-
-%% @TODO [RTI-8302] This one could be removed after the IPv4+IPv6 MMDB is definitely used
-set_is_ipv6_mmdb(Meta) ->
-    application:set_env(geodata2, ipv6, geodata2_format:is_ipv6(Meta)).
--- src/geodata2_format.erl
+++ src/geodata2_format.erl
@@ -33,7 +33,7 @@
 -define(EX_GEO_FLOAT, 15).
 
 %% API
--export([parse/2, unpack/2, meta/1, lookup/4, is_ipv6/1]).
+-export([parse/2, unpack/2, meta/1, lookup/4]).
 
 meta(Data) ->
     Size = byte_size(Data),
@@ -111,12 +111,6 @@ v4_tree_start(Data, #meta{ip_version = 6} = Meta) ->
     {error, {partial, Pos}} = lookup(Meta, Data, Bits, 6),
     Pos.
 
-%% @TODO [RTI-8302] This one could be removed after the IPv4+IPv6 MMDB is definitely used
-is_ipv6(#meta{ip_version = 6}) ->
-    true;
-is_ipv6(_) ->
-    false.
-
 lookup(#meta{ip_version = V} = Meta,
        Data,
        Bits,
@@ -126,13 +120,7 @@ lookup(#meta{ip_version = 6, v4_start = V4Start} = Meta,
        Data,
        Bits,
        4) -> %%lookup v4 in v6 db
-    lookup_pos(Meta, Data, Bits, V4Start);
-%% @TODO [RTI-8302] This one could be removed after the IPv4+IPv6 MMDB is definitely used
-lookup(_,
-       _,
-       _,
-       _) -> %%lookup v6 in v4 db
-    {error, v4db}.
+    lookup_pos(Meta, Data, Bits, V4Start).
 
 %% can't happen in the real db, gets used for lookup of v4 tree start
 lookup_pos(#meta{node_count = NodeCount}, _, <<>>, Pos) when Pos < NodeCount ->
